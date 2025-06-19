3a390404e59caf8acd564f9ddb9d3cab25eb45b7
Nirmal Jaiswal <nirmaljaiswal888@gmail.com>
2025-06-19 23:12:23 +0530

    Added ModuleWrapper project

---
+++ ModuleWrapper/index.js
@@ -0,0 +1,7 @@
+//Module Wrapper
+(function(exports, require, module, __filename, __dirname) {
+  // your code here
+  // console.log("Inside wrapper");
+})(); // this calls the function
+console.log(__dirname);
+console.log(__filename);
